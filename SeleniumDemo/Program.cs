f903e9701e7f6bf5217115db88598f29b26f779b
Shaun Martin <webmaster@ginblades.com>
2017-03-06 21:02:07 -0600

    Basic Selenium demo

--- SeleniumDemo/Program.cs
+++ SeleniumDemo/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
 
 namespace SeleniumDemo
 {
@@ -10,6 +10,19 @@ namespace SeleniumDemo
     {
         static void Main(string[] args)
         {
+            // Found driver at https://sites.google.com/a/chromium.org/chromedriver/downloads
+            using (IWebDriver driver = new ChromeDriver(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName))
+            {
+                driver.Navigate().GoToUrl("http://www.google.com/");
+                IWebElement query = driver.FindElement(By.Name("q"));
+                query.SendKeys("Cheese");
+                query.Submit();
+                // Fails with WebDriver 3.2, using 2.27
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+                Console.WriteLine("Page title is: " + driver.Title);
+
+            }
         }
     }
 }
